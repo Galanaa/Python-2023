ae96ef374adb8faa6092fb1bd05102bbaa4a38fe
Galanaa <131612661+Galanaa@users.noreply.github.com>
2023-05-14 21:17:32 +0200

    "upload all"

---
+++ Lesson_3..py
@@ -0,0 +1,6 @@
+first = 1980
+last = 2021
+while first < last:
+    if first % 4 == 0:
+        print("first")
+    first = +1
---
+++ Lesson_3.py
@@ -0,0 +1,17 @@
+temperature = float(input("the temperature today: "))
+
+
+if temperature < 12 and temperature >= 10:
+    print("Wear a jacket")
+
+if temperature < 6:
+    print(" wear a pair of gloves")
+
+if temperature < 10 and temperature >= 8:
+    print("to wear a scarf")
+
+if temperature < 8:
+    print("wear a hat")
+
+if temperature > 25:
+    print(" wear a sunglasses")
